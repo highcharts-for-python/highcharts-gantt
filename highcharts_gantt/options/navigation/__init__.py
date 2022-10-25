5dfeb2cc7c964cdf18ee1547f567fc296f7bfeb9
Chris Modzelewski <software@insightindustry.com>
2022-10-25 19:28:10 -0400

    Implemented options.navigation.

--- highcharts_gantt/options/navigation/__init__.py
+++ highcharts_gantt/options/navigation/__init__.py
@@ -1,23 +1,3 @@
 from typing import Optional
 
-from highcharts_python.options.navigation import Navigation as NavigationBase
-
-from highcharts_stock.decorators import class_sensitive
-from highcharts_stock.options.annotations import NavigationAnnotationOptions
-
-
-class Navigation(NavigationBase):
-
-    @property
-    def annotation_options(self) -> Optional[NavigationAnnotationOptions]:
-        """Additional options to be applied to all annotations.
-
-        :rtype: :class:`NavigationAnnotationOptions <highcharts_stock.options.annotations.NavigationAnnotationOptions>`
-          or :obj:`None <python:None>`
-        """
-        return self._annotation_options
-
-    @annotation_options.setter
-    @class_sensitive(NavigationAnnotationOptions)
-    def annotation_options(self, value):
-        self._annotation_options = value
+from highcharts_python.options.navigation import *
