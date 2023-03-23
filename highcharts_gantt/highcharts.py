e4b2db09a3b0e00cdceaa027f8ba62fb18cdcd95
Chris Modzelewski <chris@highchartspython.com>
2023-03-23 17:30:43 -0400

    Updated highcharts_gantt.highcharts.

--- highcharts_gantt/highcharts.py
+++ highcharts_gantt/highcharts.py
@@ -1,115 +1,116 @@
 """Convenience module that imports all classes from the entire library."""
 
-from highcharts_maps.chart import *
-from highcharts_maps.options import *
-from highcharts_maps.headless_export import *
-from highcharts_maps.global_options import *
-from highcharts_maps.global_options.language import *
-from highcharts_maps.global_options.shared_options import *
-from highcharts_maps.global_options.language.accessibility import *
-from highcharts_maps.options.accessibility import *
-from highcharts_maps.options.annotations import *
-from highcharts_maps.options.axes import *
-from highcharts_maps.options.chart import *
-from highcharts_maps.options.exporting import *
-from highcharts_maps.options.legend import *
-from highcharts_maps.options.navigation import *
-from highcharts_maps.options.plot_options import *
-from highcharts_maps.options.series import *
-from highcharts_maps.options.series.data import *
+from highcharts_gantt.chart import *
+from highcharts_gantt.options import *
+from highcharts_gantt.headless_export import *
+from highcharts_gantt.global_options import *
+from highcharts_gantt.global_options.language import *
+from highcharts_gantt.global_options.shared_options import *
+from highcharts_gantt.global_options.language.accessibility import *
+from highcharts_gantt.options.accessibility import *
+from highcharts_gantt.options.annotations import *
+from highcharts_gantt.options.axes import *
+from highcharts_gantt.options.chart import *
+from highcharts_gantt.options.exporting import *
+from highcharts_gantt.options.legend import *
+from highcharts_gantt.options.navigation import *
+from highcharts_gantt.options.plot_options import *
+from highcharts_gantt.options.series import *
+from highcharts_gantt.options.series.data import *
 
-from highcharts_maps.options.series.arcdiagram import ArcDiagramSeries
-from highcharts_maps.options.series.area import AreaSeries
-from highcharts_maps.options.series.area import AreaRangeSeries
-from highcharts_maps.options.series.area import AreaSplineSeries
-from highcharts_maps.options.series.area import AreaSplineRangeSeries
-from highcharts_maps.options.series.bar import BarSeries
-from highcharts_maps.options.series.bellcurve import BellCurveSeries
-from highcharts_maps.options.series.boxplot import BoxPlotSeries
-from highcharts_maps.options.series.bubble import BubbleSeries
-from highcharts_maps.options.series.bullet import BulletSeries
-from highcharts_maps.options.series.bar import ColumnSeries
-from highcharts_maps.options.series.bar import ColumnPyramidSeries
-from highcharts_maps.options.series.bar import ColumnRangeSeries
-from highcharts_maps.options.series.bar import CylinderSeries
-from highcharts_maps.options.series.dependencywheel import DependencyWheelSeries
-from highcharts_maps.options.series.dumbbell import DumbbellSeries
-from highcharts_maps.options.series.boxplot import ErrorBarSeries
-from highcharts_maps.options.series.funnel import FunnelSeries
-from highcharts_maps.options.series.funnel import Funnel3DSeries
-from highcharts_maps.options.series.gauge import GaugeSeries
-from highcharts_maps.options.series.heatmap import HeatmapSeries
-from highcharts_maps.options.series.histogram import HistogramSeries
-from highcharts_maps.options.series.item import ItemSeries
-from highcharts_maps.options.series.area import LineSeries
-from highcharts_maps.options.series.dumbbell import LollipopSeries
-from highcharts_maps.options.series.networkgraph import NetworkGraphSeries
-from highcharts_maps.options.series.organization import OrganizationSeries
-from highcharts_maps.options.series.packedbubble import PackedBubbleSeries
-from highcharts_maps.options.series.pareto import ParetoSeries
-from highcharts_maps.options.series.pie import PieSeries
-from highcharts_maps.options.series.polygon import PolygonSeries
-from highcharts_maps.options.series.pyramid import (PyramidSeries,
+from highcharts_gantt.options.series.arcdiagram import ArcDiagramSeries
+from highcharts_gantt.options.series.area import AreaSeries
+from highcharts_gantt.options.series.area import AreaRangeSeries
+from highcharts_gantt.options.series.area import AreaSplineSeries
+from highcharts_gantt.options.series.area import AreaSplineRangeSeries
+from highcharts_gantt.options.series.bar import BarSeries
+from highcharts_gantt.options.series.bellcurve import BellCurveSeries
+from highcharts_gantt.options.series.boxplot import BoxPlotSeries
+from highcharts_gantt.options.series.bubble import BubbleSeries
+from highcharts_gantt.options.series.bullet import BulletSeries
+from highcharts_gantt.options.series.bar import ColumnSeries
+from highcharts_gantt.options.series.bar import ColumnPyramidSeries
+from highcharts_gantt.options.series.bar import ColumnRangeSeries
+from highcharts_gantt.options.series.bar import CylinderSeries
+from highcharts_gantt.options.series.dependencywheel import DependencyWheelSeries
+from highcharts_gantt.options.series.dumbbell import DumbbellSeries
+from highcharts_gantt.options.series.boxplot import ErrorBarSeries
+from highcharts_gantt.options.series.funnel import FunnelSeries
+from highcharts_gantt.options.series.funnel import Funnel3DSeries
+from highcharts_gantt.options.series.gantt import GanttSeries
+from highcharts_gantt.options.series.gauge import GaugeSeries
+from highcharts_gantt.options.series.heatmap import HeatmapSeries
+from highcharts_gantt.options.series.histogram import HistogramSeries
+from highcharts_gantt.options.series.item import ItemSeries
+from highcharts_gantt.options.series.area import LineSeries
+from highcharts_gantt.options.series.dumbbell import LollipopSeries
+from highcharts_gantt.options.series.networkgraph import NetworkGraphSeries
+from highcharts_gantt.options.series.organization import OrganizationSeries
+from highcharts_gantt.options.series.packedbubble import PackedBubbleSeries
+from highcharts_gantt.options.series.pareto import ParetoSeries
+from highcharts_gantt.options.series.pie import PieSeries
+from highcharts_gantt.options.series.polygon import PolygonSeries
+from highcharts_gantt.options.series.pyramid import (PyramidSeries,
                                                      Pyramid3DSeries)
-from highcharts_maps.options.series.sankey import SankeySeries
-from highcharts_maps.options.series.scatter import (ScatterSeries,
+from highcharts_gantt.options.series.sankey import SankeySeries
+from highcharts_gantt.options.series.scatter import (ScatterSeries,
                                                      Scatter3DSeries)
-from highcharts_maps.options.series.gauge import SolidGaugeSeries
-from highcharts_maps.options.series.spline import SplineSeries
-from highcharts_maps.options.series.area import StreamGraphSeries
-from highcharts_maps.options.series.sunburst import SunburstSeries
-from highcharts_maps.options.series.heatmap import TilemapSeries
-from highcharts_maps.options.series.timeline import TimelineSeries
-from highcharts_maps.options.series.treemap import TreemapSeries
-from highcharts_maps.options.series.pie import VariablePieSeries
-from highcharts_maps.options.series.bar import VariwideSeries
-from highcharts_maps.options.series.vector import VectorSeries
-from highcharts_maps.options.series.venn import VennSeries
-from highcharts_maps.options.series.bar import WaterfallSeries
-from highcharts_maps.options.series.bar import WindBarbSeries
-from highcharts_maps.options.series.wordcloud import WordcloudSeries
-from highcharts_maps.options.series.bar import XRangeSeries
+from highcharts_gantt.options.series.gauge import SolidGaugeSeries
+from highcharts_gantt.options.series.spline import SplineSeries
+from highcharts_gantt.options.series.area import StreamGraphSeries
+from highcharts_gantt.options.series.sunburst import SunburstSeries
+from highcharts_gantt.options.series.heatmap import TilemapSeries
+from highcharts_gantt.options.series.timeline import TimelineSeries
+from highcharts_gantt.options.series.treemap import TreemapSeries
+from highcharts_gantt.options.series.pie import VariablePieSeries
+from highcharts_gantt.options.series.bar import VariwideSeries
+from highcharts_gantt.options.series.vector import VectorSeries
+from highcharts_gantt.options.series.venn import VennSeries
+from highcharts_gantt.options.series.bar import WaterfallSeries
+from highcharts_gantt.options.series.bar import WindBarbSeries
+from highcharts_gantt.options.series.wordcloud import WordcloudSeries
+from highcharts_gantt.options.series.bar import XRangeSeries
 
 # Highcharts Stock Plot Series
-from highcharts_maps.options.series.abands import (AbandsSeries,
+from highcharts_gantt.options.series.abands import (AbandsSeries,
                                                     PCSeries,
                                                     KeltnerChannelsSeries,
                                                     BBSeries)
-from highcharts_maps.options.series.ad import ADSeries
-from highcharts_maps.options.series.aroon import AroonSeries
-from highcharts_maps.options.series.atr import ATRSeries, NATRSeries
-from highcharts_maps.options.series.averages import (DEMASeries,
+from highcharts_gantt.options.series.ad import ADSeries
+from highcharts_gantt.options.series.aroon import AroonSeries
+from highcharts_gantt.options.series.atr import ATRSeries, NATRSeries
+from highcharts_gantt.options.series.averages import (DEMASeries,
                                                       EMASeries,
                                                       SMASeries,
                                                       TEMASeries,
                                                       VWAPSeries,
                                                       WMASeries)
-from highcharts_maps.options.series.candlestick import (CandlestickSeries,
+from highcharts_gantt.options.series.candlestick import (CandlestickSeries,
                                                          HollowCandlestickSeries,
                                                          HeikinAshiSeries)
-from highcharts_maps.options.series.disparity_index import DisparityIndexSeries
-from highcharts_maps.options.series.dmi import DMISeries
-from highcharts_maps.options.series.flags import FlagsSeries
-from highcharts_maps.options.series.hlc import (HLCSeries,
+from highcharts_gantt.options.series.disparity_index import DisparityIndexSeries
+from highcharts_gantt.options.series.dmi import DMISeries
+from highcharts_gantt.options.series.flags import FlagsSeries
+from highcharts_gantt.options.series.hlc import (HLCSeries,
                                                  OHLCSeries)
-from highcharts_maps.options.series.linear_regressions import (LinearRegressionSeries,
+from highcharts_gantt.options.series.linear_regressions import (LinearRegressionSeries,
                                                                 LinearRegressionAngleSeries,
                                                                 LinearRegressionInterceptSeries,
                                                                 LinearRegressionSlopeSeries,
                                                                 TrendlineSeries)
-from highcharts_maps.options.series.pivot_points import PivotPointsSeries
-from highcharts_maps.options.series.price_envelopes import PriceEnvelopesSeries
-from highcharts_maps.options.series.psar import PSARSeries
-from highcharts_maps.options.series.vbp import VBPSeries
-from highcharts_maps.options.series.zigzag import ZigZagSeries
-from highcharts_maps.options.series.momentum import (MomentumSeries,
+from highcharts_gantt.options.series.pivot_points import PivotPointsSeries
+from highcharts_gantt.options.series.price_envelopes import PriceEnvelopesSeries
+from highcharts_gantt.options.series.psar import PSARSeries
+from highcharts_gantt.options.series.vbp import VBPSeries
+from highcharts_gantt.options.series.zigzag import ZigZagSeries
+from highcharts_gantt.options.series.momentum import (MomentumSeries,
                                                       OBVSeries,
                                                       ROCSeries,
                                                       RSISeries)
-from highcharts_maps.options.series.momentum.ikh import IKHSeries
-from highcharts_maps.options.series.momentum.macd import MACDSeries
-from highcharts_maps.options.series.momentum.supertrend import SupertrendSeries
-from highcharts_maps.options.series.oscillators import (AroonOscillatorSeries,
+from highcharts_gantt.options.series.momentum.ikh import IKHSeries
+from highcharts_gantt.options.series.momentum.macd import MACDSeries
+from highcharts_gantt.options.series.momentum.supertrend import SupertrendSeries
+from highcharts_gantt.options.series.oscillators import (AroonOscillatorSeries,
                                                          APOSeries,
                                                          CCISeries,
                                                          ChaikinSeries,
@@ -117,12 +118,12 @@ from highcharts_maps.options.series.oscillators import (AroonOscillatorSeries,
                                                          DPOSeries,
                                                          TRIXSeries,
                                                          WilliamsRSeries)
-from highcharts_maps.options.series.oscillators.ao import AOSeries
-from highcharts_maps.options.series.oscillators.klinger import KlingerSeries
-from highcharts_maps.options.series.oscillators.money_flow import (MFISeries,
+from highcharts_gantt.options.series.oscillators.ao import AOSeries
+from highcharts_gantt.options.series.oscillators.klinger import KlingerSeries
+from highcharts_gantt.options.series.oscillators.money_flow import (MFISeries,
                                                                     CMFSeries)
-from highcharts_maps.options.series.oscillators.ppo import PPOSeries
-from highcharts_maps.options.series.oscillators.stochastic import (StochasticSeries,
+from highcharts_gantt.options.series.oscillators.ppo import PPOSeries
+from highcharts_gantt.options.series.oscillators.stochastic import (StochasticSeries,
                                                                     SlowStochasticSeries)
 
-from highcharts_maps.utility_classes import *
+from highcharts_gantt.utility_classes import *
