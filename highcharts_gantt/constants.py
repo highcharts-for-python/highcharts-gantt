328fb65e6ab8d20e1e1c2938df9f4ab500005865
Chris Modzelewski <chris@highchartspython.com>
2023-04-01 23:31:04 -0400

    Fixed JS script inclusion.

--- highcharts_gantt/constants.py
+++ highcharts_gantt/constants.py
@@ -1,10 +1,10 @@
 """Defines a set of constants that are used throughout the library."""
 from highcharts_stock.constants import *
 
-
-GANTT_INCLUDE_LIBS = STOCK_INCLUDE_LIBS + [
+GANTT_INCLUDE_LIBS = [x for x in STOCK_INCLUDE_LIBS]
+GANTT_INCLUDE_LIBS.extend([
     "https://code.highcharts.com/gantt/modules/gantt.js",
-]
+])
 
 GANTT_INCLUDE_STR = STOCK_INCLUDE_STR + """
     <script src="https://code.highcharts.com/gantt/modules/gantt.js"/>
